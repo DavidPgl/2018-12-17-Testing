0a588d84c970f05025312b091b21906c67391097
DavidP <panagiotopulos.d@gmail.com>
2019-01-03 14:10:21 +0100

    Initial commit

---
+++ src/AbsoluteTest.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class AbsoluteTest {
+
+
+   @Test
+    void positiveZero(){
+        assertEquals(0,Absolute.absoluteValueOf(0));
+    }
+    @Test
+    void negativeZero(){
+        assertEquals(0,Absolute.absoluteValueOf(-0));
+    }
+    @Test
+    void positiveOne(){
+        assertEquals(1,Absolute.absoluteValueOf(1));
+    }
+    @Test
+    void negativeOne(){
+        assertEquals(1,Absolute.absoluteValueOf(-1));
+    }
+    @Test
+    void positiveOdd(){
+        assertEquals(5,Absolute.absoluteValueOf(5));
+    }
+    @Test
+    void negativeOdd(){
+        assertEquals(5,Absolute.absoluteValueOf(-5));
+    }
+    @Test
+    void positiveEven(){
+        assertEquals(2,Absolute.absoluteValueOf(2));
+    }
+    @Test
+    void negativeEven(){
+        assertEquals(2,Absolute.absoluteValueOf(-2));
+    }
+    @Test
+    void bigPositive(){
+        assertEquals(1768,Absolute.absoluteValueOf(1768));
+    }
+    @Test
+    void bigNegative(){
+        assertEquals(1022,Absolute.absoluteValueOf(-1022));
+    }
+}
